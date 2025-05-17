e16a6074b69c422791dfba541541162d6f5feef3
Dolly <rajdolly1999@gmail.com>
2025-05-17 22:17:59 +0530

    Added BFS algorithm

---
+++ src/Graph/BFS.java
@@ -0,0 +1,76 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    /*
+    Queue data structure: follows FIFO, and will always contain the starting.
+--->Visited array: an array initialized to 0
+--->In BFS, we start with a “starting” node, mark it as visited,
+and push it into the queue data structure.
+--->In every iteration, we pop out the node ‘v’ and put it in the solution vector,
+as we are traversing this node.
+--->All the unvisited adjacent nodes from ‘v’ are visited next
+and are pushed into the queue. The list of adjacent neighbors of the node
+can be accessed from the adjacency list.
+--->Repeat steps 2 and 3 until the queue becomes empty,
+and this way you can easily traverse all the nodes in the graph.
+-->In this way, all the nodes are traversed in a breadthwise manner.
+     */
+
+    public static void bfs(int[] visited,ArrayList<ArrayList<Integer>> adjList){
+
+        Queue<Integer> queue=new LinkedList<>();
+
+        queue.add(0);
+        //mark node as visited
+        visited[0]=1;
+
+        while(!queue.isEmpty()){
+           int curr= queue.poll();//retrieves and removes the head node
+            System.out.print(curr+" ");
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+          for(int nbr: adjList.get(curr)){
+              if(visited[nbr]==0) {
+                  visited[nbr] = 1;
+                  queue.add(nbr);
+              }
+          }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph={{0,1},{0,2},{1,2},{2,3}};
+        //from vertex u to v (u,v)
+
+        ArrayList<ArrayList<Integer>> adjList=new ArrayList<ArrayList<Integer>>();
+        //V=4 no of vertices
+        for(int i=0;i<4;i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        //no of edges=graph.length
+        //no of nodes=4
+       /* for(int i=0;i<4;i++){
+            for(int j=0;j< graph.length;j++){
+                int u=graph[j][0];
+                int v=graph[j][1];
+                adjList.get(u).add(v);
+
+            }
+        }*/
+        for(int[] edge:graph){
+            int u=edge[0];
+            int v=edge[1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+        int[] visited=new int[adjList.size()];
+        bfs(visited,adjList);
+    }
+}
--- src/Graph/DFS.java
+++ src/Graph/DFS.java
@@ -16,21 +16,47 @@ SC--> O(n)--> Maximum size of recursive stack
     - then terminate.
     """
  */
-    public void DFS(ArrayList<Integer>[] graph,int src,boolean[] visited){
+    public static void dfs(ArrayList<ArrayList<Integer>> graph,int src,boolean[] visited){
         //Mark visited
         visited[src]=true;
         System.out.println(src);
 
         //explore its all neighbour
 
-        for(int nbr:graph[src]){
-            if(visited[nbr]==false){
-                DFS(graph,nbr,visited);
+        for(int nbr:graph.get(src)){
+            if(!visited[nbr]){
+                dfs(graph,nbr,visited);
             }
         }
     }
 
     public static void main(String[] args) {
+        int[][] graph={{0,1},{0,2},{1,2},{2,3}};
+        //from vertex u to v (u,v)
 
+        ArrayList<ArrayList<Integer>> adjList=new ArrayList<ArrayList<Integer>>();
+        //V=4 no of vertices
+        for(int i=0;i<4;i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        //no of edges=graph.length
+        //no of nodes=4
+       /* for(int i=0;i<4;i++){
+            for(int j=0;j< graph.length;j++){
+                int u=graph[j][0];
+                int v=graph[j][1];
+                adjList.get(u).add(v);
+
+            }
+        }*/
+        for(int[] edge:graph){
+            int u=edge[0];
+            int v=edge[1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+        boolean[] visited=new boolean[adjList.size()];
+        dfs(adjList,0,visited);
     }
 }
